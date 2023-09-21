a2d760d812c20599e041f69b3a757ac9ab754665
SUJIT SWAIN <90376564+Sujitswain@users.noreply.github.com>
2023-09-21 12:10:43 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Quiz from './components/Quizzical'
+
+function App() {
+  return (
+    <div>
+        <Quiz />
+    </div>
+  );
+}
+
+export default App;
